38337fedb6583ad5b613e71f838dbcb7277a6949
KD Puvvadi <rao@puvvadi.me>
2024-02-19 13:35:45 +0530

    bio

--- data/BioData.js
+++ data/BioData.js
@@ -1,25 +1,23 @@
 // date: 1 Aug, 2022
 const bioData = [
     {
-        name: 'nxt lnk',
-        username: 'nxtlnk.vercel.app',
-        url: 'https://nxtlnk.vercel.app',
+        name: 'KD Puvvadi',
+        username: 'kdpuvvadi.vercel.app',
+        url: 'https://kdpuvvadi.vercel.app',
         titleImg: true,
-        avatar: '/avatar.png',
+        avatar: '/images/avatar.webp',
         nftAvatar: true,
-        description: 'Custom bio links for creatives who love coding. Made this first to use for my own weblinks vjy.me/lnk. ',
+        description: 'Software, DevOps and Magic',
         descShow: true,
-        subdesc: 'Use this template and deploy on vercel for free',
-        subdescShow: true,
-        newProductUrl: 'https://3dicons.co',
-        newProduct: true,
-        author: 'realvjy',
-        authorURL: 'https://twitter.com/realvjy',
-        footerText: 'made by'
+        subdesc: '',
+        subdescShow: false,
+        newProductUrl: '',
+        newProduct: false,
+        author: '',
+        authorURL: 'https://twitter.com/kdpuvvadi',
+        footerText: 'made by',
+        footerShow: false
     },
 ];
 
 export default bioData;
-
-
-
