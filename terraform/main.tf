746ce650ab5234ee6fde5bb32af07ff60050ad52
KD Puvvadi <rao@puvvadi.me>
2024-02-21 13:28:42 +0530

    deploy site on vercel with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
       version = "4.24.0"
     }
     vercel = {
-      source = "vercel/vercel"
+      source  = "vercel/vercel"
       version = "1.1.0"
     }
   }
@@ -33,3 +33,58 @@ data "cloudflare_zones" "zone_puvvadi_me" {
   }
 }
 
+resource "vercel_project" "puvvadi_me_project" {
+  name      = "puvvadi-me"
+  framework = "nextjs"
+
+  git_repository = {
+    type = "github"
+    repo = "kdpuvvadi/puvvadi_me"
+  }
+}
+
+resource "vercel_deployment" "puvvadi_me_deploy" {
+  project_id = vercel_project.puvvadi_me_project.id
+  ref        = "main"
+  production = "true"
+
+}
+
+resource "cloudflare_record" "puvvadi_me_record" {
+  zone_id         = data.cloudflare_zones.zone_puvvadi_me.zones[0].id
+  name            = "@"
+  value           = vercel_deployment.puvvadi_me_deploy.domains[1]
+  type            = "CNAME"
+  ttl             = 1
+  allow_overwrite = true
+  proxied         = true
+}
+
+resource "vercel_project_domain" "puvvadi_me_domain" {
+  project_id = vercel_project.puvvadi_me_project.id
+  domain     = "puvvadi.me"
+}
+
+output "deploy_domain" {
+  value = vercel_deployment.puvvadi_me_deploy.domains
+}
+
+resource "cloudflare_record" "puvvadi_me_record_www" {
+  zone_id         = data.cloudflare_zones.zone_puvvadi_me.zones[0].id
+  name            = "www"
+  value           = "puvvadi.me"
+  type            = "CNAME"
+  ttl             = 1
+  allow_overwrite = true
+  proxied         = true
+}
+
+resource "cloudflare_record" "puvvadi_me_record_kd" {
+  zone_id         = data.cloudflare_zones.zone_puvvadi_me.zones[0].id
+  name            = "kd"
+  value           = "puvvadi.me"
+  type            = "CNAME"
+  ttl             = 1
+  allow_overwrite = true
+  proxied         = true
+}
